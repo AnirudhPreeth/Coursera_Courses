545bb135127ebe13fed700fcb2bf4b0dbdb5542d
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-03-05 00:00:26 +0530

    Update.

---
+++ Google Data Analytics Professional Certificate/1.py
@@ -0,0 +1,163 @@
+#Welcome to the Google Data Analytics Certificate.
+
+'''
+Data is a collection of facts. This collection can include numbers, pictures, videos, words, measurements, observations, and more. 
+Once you have data, analytics puts it to work through analysis. 
+
+Data analysis is the collection, transformation, and organization of data in order to draw conclusions, make predictions,...
+and drive informed decision-making. And it doesn't stop there. 
+Data evolves over time which means this analysis or analytics, as we call it, can give us new information throughout data's...
+...entire life cycle. Data is everywhere. You use and create data everyday.
+
+Ask, Prepare, Process, Analyze, Share, Act.
+
+"What design features will make ou packaging easier to recycle?"
+
+Foundations: Data, Data, Everywhere.
+Ask Questions to Make Data-Driven Decisions
+Prepare Data for Exploration
+Process Data from Dirty to Clean
+Analyze Data to Answer Questions
+Share Data Through the Art of Visualization 
+Data Analysis with R Programming 
+Google Data Analytics Capstone: Complete a Case Study 
+
+Data analytics is the science of data.
+
+FOUNDATION -
+What you will learn:
+Real-life roles and responsibilities of a junior data analyst
+How businesses transform data into actionable insights
+Spreadsheet basics
+Database and query basics
+Data visualization basics
+
+Skill sets you will build:
+Using data in everyday life
+Thinking analytically
+Applying tools from the data analytics toolkit
+Showing trends and patterns with data visualizations
+Ensuring your data analysis is fair
+
+ASK-
+What you will learn:
+How data analysts solve problems with data
+The use of analytics for making data-driven decisions
+Spreadsheet formulas and functions
+Dashboard basics, including an introduction to Tableau
+Data reporting basics
+
+Skill sets you will build:
+Asking SMART and effective questions
+Structuring how you think
+Summarizing data
+Putting things into context
+Managing team and stakeholder expectations
+Problem-solving and conflict-resolution
+
+PREPARE-
+What you will learn:
+How data is generated
+Features of different data types, fields, and values
+Database structures
+The function of metadata in data analytics
+Structured Query Language (SQL) functions
+
+Skill sets you will build:
+Ensuring ethical data analysis practices
+Addressing issues of bias and credibility
+Accessing databases and importing data
+Writing simple queries
+Organizing and protecting data
+Connecting with the data community (optional)
+
+PROCESS-
+What you will learn:
+Data integrity and the importance of clean data
+The tools and processes used by data analysts to clean data
+Data-cleaning verification and reports
+Statistics, hypothesis testing, and margin of error
+Resume building and interpretation of job postings (optional)
+
+Skill sets you will build:
+Connecting business objectives to data analysis
+Identifying clean and dirty data
+Cleaning small datasets using spreadsheet tools
+Cleaning large datasets by writing SQL queries
+Documenting data-cleaning processes
+
+ANALYZE- 
+What you will learn:
+Steps data analysts take to organize data
+How to combine data from multiple sources
+Spreadsheet calculations and pivot tables
+SQL calculations
+Temporary tables
+Data validation
+
+Skill sets you will build:
+Sorting data in spreadsheets and by writing SQL queries
+Filtering data in spreadsheets and by writing SQL queries
+Converting data
+Formatting data
+Substantiating data analysis processes
+Seeking feedback and support from others during data analysis
+
+SHARE-
+What you will learn:
+Design thinking
+How data analysts use visualizations to communicate about data
+The benefits of Tableau for presenting data analysis findings
+Data-driven storytelling
+Dashboards and dashboard filters
+Strategies for creating an effective data presentation
+
+Skill sets you will build:
+Creating visualizations and dashboards in Tableau
+Addressing accessibility issues when communicating about data
+Understanding the purpose of different business communication tools
+Telling a data-driven story
+Presenting to others about data
+Answering questions about data
+
+ACT-
+What you will learn:
+Programming languages and environments
+R packages
+R functions, variables, data types, pipes, and vectors
+R data frames
+Bias and credibility in R
+R visualization tools
+R Markdown for documentation, creating structure, and emphasis
+
+Skill sets you will build:
+Coding in R
+Writing functions in R
+Accessing data in R
+Cleaning data in R
+Generating data visualizations in R
+Reporting on data analysis to stakeholders
+
+CAPSTONE-
+What you will learn:
+How a data analytics portfolio distinguishes you from other candidates
+Practical, real-world problem-solving
+Strategies for extracting insights from data
+Clear presentation of data findings
+Motivation and ability to take initiative
+
+Skill sets you will build:
+Building a portfolio
+Increasing your employability
+Showcasing your data analytics knowledge, skill, and technical expertise
+Sharing your work during an interview
+Communicating your unique value proposition to a potential employer
+
+Turning Data into insights. 
+The six steps of the data analysis process that you have been learning in this program are: ask, prepare, process, analyze, share, and act. 
+These six steps apply to any data analysis.
+Ask, preparation, data(collected, stored, managed, protected), discovered, document, sharing the report, communicate the results, implement changes and take actions.
+
+Dimensions of data analytics-
+
+'''
